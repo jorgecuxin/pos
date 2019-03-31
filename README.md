5da2a4ff11c332b5e488e7302fac4246de0be991
Jorge Cuxin <jorgecuxin@gmail.com>
2019-03-30 23:12:04 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+## Punto de venta
+
+
+
+Sistema de punto de venta en PHP.
+
+
+
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2019 <a href="https://www.facebook.com/CEO.DigitalPlus/" target="_blank">Digital Plus</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
